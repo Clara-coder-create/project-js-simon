9d5ae3423744c2993128d5f991d0abc309ef0cb3
Clara-coder-create <cschilling@brynmawr.edu>
2021-06-21 12:38:19 +0200

    added sound when buttons are clicked

--- site/buttons.js
+++ site/buttons.js
@@ -1,7 +1,30 @@
 let greenButton = document.querySelector('.simon-button.green');
 greenButton.addEventListener('click', function() {
   alert('You clicked the green button');
+  var onclick: (this: Window, ev: MouseEvent) => document.getElementById('mouse-click').play()
 });
+
+/* Trying to add sound on click to the green button
+
+var greenButton = document.getElementbyId("green-button");
+greenButton.addEventListener('click',playSound);
+
+var sound = new Audio ();
+sound.src = "Mouse-click.mp3";
+sound.oncamplaythough = function(){
+     sound.readyToRock = ture;
+};
+sound.onerror = function (){
+      console.log("Sound file Mouse-click.mp3 has failed to load")
+};
+
+var audio = new Audio("Mouse-click.mp3");
+let greenButton = document.querySelector('.simon-button.green');
+greenButton.addEventListener('click', function() {
+  audio.play();
+}*/
+
+
 
 let redButton = document.querySelector('.simon-button.red');
 redButton.addEventListener('click', function() {
